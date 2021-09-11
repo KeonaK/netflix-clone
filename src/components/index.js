0009ed94e6d2758343265479b8ea40bea4b451c2
Keona Kirby <keonakirby@gmail.com>
2021-09-11 16:40:28 -0500

    styled the signin form page and the components now display on the browser

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as EmailForm } from "./email-form";
 export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as Feature } from "./feature";
+export { default as Form } from "./form";
