82a1ca401b3f3f1daa2169502a3185e779d7bf53
Keona Kirby <keonakirby@gmail.com>
2021-09-10 21:31:33 -0500

    added seed file and fire base config key

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,7 +5,14 @@ import "firebase/auth";
 // need to seed the databasae
 
 //config
-const config = {};
+const config = {
+  apiKey: "AIzaSyAI6UslRjNarcW809rCzv5PoWRYJn6FuW0",
+  authDomain: "netflix-clone-63da4.firebaseapp.com",
+  projectId: "netflix-clone-63da4",
+  storageBucket: "netflix-clone-63da4.appspot.com",
+  messagingSenderId: "257969111947",
+  appId: "1:257969111947:web:5b3edb1c1caee4b5e6258e",
+};
 const firebase = Firebase.initializeApp(config);
 
 export { firebase };
