3f91ddf6159c19dc219a3f67fe7cf29dae13d018
Keona Kirby <keonakirby@gmail.com>
2021-09-13 13:30:52 -0500

    added switch to App.js to help with redirection

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -13,10 +13,37 @@ export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
 
         if (user) {
           // if user redirect to the loggedInPath
+
           return (
             <Redirect
               to={{
                 pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+        return null;
+      }}
+    />
+  );
+}
+
+export function ProtectedRoute({ user, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: "signin",
+                // location state not the react state
+                state: { from: location },
               }}
             />
           );
