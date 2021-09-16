0c040a89435dda9e80ed3320694907f190684aff
Keona Kirby <keonakirby@gmail.com>
2021-09-16 14:01:53 -0500

    created browse container which will hold the series and film slides with the appropriate lable

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { BrowseContainer } from "../containers/browse";
 import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
   const { series } = useContent("series");
   const { films } = useContent("films");
-  console.log(series);
-  console.log(films);
-  return <p>Hello from browse</p>;
+  const slides = selectionFilter({ series, films });
+  return <BrowseContainer slides={slides} />;
 }
