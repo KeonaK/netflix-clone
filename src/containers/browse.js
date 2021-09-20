4e47c3195badbb49b28303873270088baed2750d
Keona Kirby <keonakirby@gmail.com>
2021-09-20 18:34:20 -0500

    added a spinner animation that animates while a selected profile is loading

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SelectProfileContainer } from "./profiles";
 import { useContext, useState, useEffect } from "react";
 import { FirebaseContext } from "../context/firebase";
+import { Loading } from "../components";
 
 export function BrowseContainer({ slides }) {
   //state for the profile
@@ -17,5 +18,11 @@ export function BrowseContainer({ slides }) {
       setloading(false);
     }, 3000);
   }, [profile.displayName]);
-  return <SelectProfileContainer user={user} setProfile={setProfile} />;
+  return profile.displayName ? (
+    loading ? (
+      <Loading src={user.photoURL} />
+    ) : null
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -15,7 +15,15 @@ export function SelectProfileContainer({ user, setProfile }) {
       <Profiles>
         <Profiles.Title>Who's watching</Profiles.Title>
         <Profiles.List>
-          <Profiles.User>
+          <Profiles.User
+            //   setProfile with the displayName and photURL
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+              })
+            }
+          >
             <Profiles.Picture src={user.photoURL} />
             <Profiles.Name>{user.displayName}</Profiles.Name>
           </Profiles.User>
