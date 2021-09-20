d3226a32d5532b339c4397bc9014a5f69cf165d2
Keona Kirby <keonakirby@gmail.com>
2021-09-20 16:30:59 -0500

    fixed minor spelling errors

--- src/components/profiles/index.js
+++ src/components/profiles/index.js
@@ -1,27 +1,32 @@
 import React from "react";
-import {Title, List, User, Name} from "./styles/profiles"
+import { Title, List, User, Name } from "./styles/profiles";
 
-export default function Profiles ({children, ...restProps}){
-    return <Container {...restProps}>{children}</Container>
+export default function Profiles({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
 }
 
+Profiles.Title = function ProfilesTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
 
-Profiles.Title = function ProfilesTitle({children, ...restProps}){
-    return <Title {...restProps}>{children}</Title>
-}
+Profiles.List = function ProfilesList({ children, ...restProps }) {
+  return <List {...restProps}>{children}</List>;
+};
 
-Profiles.List = function ProfilesList({children, ...restProps}){
-    return <List {...restProps}>{children}</List>
-}
+Profiles.User = function ProfilesUser({ children, ...restProps }) {
+  return <User {...restProps}>{children}</User>;
+};
 
-Profiles.User = function ProfilesUser({children, ...restProps}){
-    return <Title {...restProps}>{children}</Title>
-}
+Profiles.Picture = function ProfilesPicture({ src, ...restProps }) {
+  //if there is no src image show the loading gif
+  return (
+    <Picture
+      {...restProps}
+      src={src ? `/images/users/${src}.png` : `/images/misc/loading.gif`}
+    />
+  );
+};
 
-Profiles.Picture = function ProfilesPicture({src, ...restProps}){
-    return <Picture {...restProps} src={src ? `/images/users/${src}.png`}/>
-}
-
-Profiles.Name = function ProfilesName({children, ...restProps}){
-    return <Name {...restProps}>{children}</Name>
-}
+Profiles.Name = function ProfilesName({ children, ...restProps }) {
+  return <Name {...restProps}>{children}</Name>;
+};
