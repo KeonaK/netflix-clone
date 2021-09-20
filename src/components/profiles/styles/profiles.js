7e33c2ed67e1f4fba1f78f49e64f7b23d206e114
Keona Kirby <keonakirby@gmail.com>
2021-09-20 16:53:41 -0500

    styled the profile components

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -1,3 +1,65 @@
 import styled from "styled-components/macro";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  max-width: 80%;
+`;
+
+export const Title = styled.h1`
+  width: 100%;
+  color: #fff;
+  font-size: 3rem;
+  text-align: center;
+  font-weight: 500;
+`;
+
+export const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Name = styled.p`
+  color: #808080;
+  text-overflow: ellipsis;
+  font-size: 1rem;
+
+  &:hover {
+    font-weight: bold;
+    color: #e5e5e5;
+  }
+`;
+
+export const Picture = styled.img`
+  width: 100%;
+  max-width: 9.375rem;
+  height: auto;
+  border: 0.1875rem solid black;
+  cursor: pointer;
+`;
+
+export const Item = styled.li`
+  max-height: 12.5rem;
+  max-width: 12.5rem;
+  list-style-type: none;
+  text-align: center;
+  margin-right: 1.875rem;
+
+  &:hover > ${Picture} {
+    border: 0.1875rem solid white;
+  }
+
+  &:hover ${Name} {
+    font-weight: bold;
+    color: white;
+  }
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
